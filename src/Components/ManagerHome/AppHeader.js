cf849abf5c9e86d80340c8e024143cf33629cd1a
Mariya <mariya.podosinova@gmail.com>
2020-06-30 19:14:55 +0100

    Home components and its children

---
+++ src/Components/ManagerHome/AppHeader.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+function AppHeader() {
+  return (
+    <nav className="d-flex flex-row-reverse p-3">
+      <Link to="/signup" className="nav-link">
+        Log Out
+      </Link>
+      <a href="#" className="nav-link ">
+        <i className="fas fa-user mr-2"></i>Welcome Brad
+      </a>
+    </nav>
+  );
+}
+
+export default AppHeader;
