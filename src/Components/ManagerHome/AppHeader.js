ff1a7a5ba8211e9a82f58b4516b528e8f84335b4
zainab.alkaleefa <zainab.alkaleefa@gmail.com>
2020-07-03 02:22:59 +0100

    Nav is responsive and doesn't collapse

--- src/Components/ManagerHome/AppHeader.js
+++ src/Components/ManagerHome/AppHeader.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 function AppHeader() {
   return (
     <nav className="d-flex flex-row-reverse p-3">
-      <Link to="/signup" className="nav-link">
+      <Link to="/sign-in" className="nav-link">
         Log Out
       </Link>
       <a href="#" className="nav-link ">
