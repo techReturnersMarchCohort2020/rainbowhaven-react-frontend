3efc3b1b673bf62dd13f3d787c4d429089322a33
zainab.alkaleefa <zainab.alkaleefa@gmail.com>
2020-07-10 02:03:49 +0100

    delivery button added to the clients and linked to volunteer pickup page, deliver button of volunteer pickup page linked the manager home

--- src/Components/ManagerHome/ListClients.js
+++ src/Components/ManagerHome/ListClients.js
@@ -1,5 +1,7 @@
 import React from "react";
 import moment from "moment";
+import { useHistory } from "react-router-dom";
+
 
 function ListClients(props) {
   function handleDeleteClick() {
@@ -10,6 +12,15 @@ function ListClients(props) {
     console.log("The complete button has been clicked!");
     props.completeDelivery(props.id);
   }
+  const history = useHistory();
+
+  function handleDeliverClick(event) {
+    event.preventDefault();
+    let path = "/pickUp-Volunteer";
+    history.push(path);
+
+
+  }
   return (
     <div className="col-sm-12 col-md-6 col-lg-3">
       <div className="Item__card card text-center m-2">
@@ -17,42 +28,42 @@ function ListClients(props) {
           <h4
             className={`${
               props.completed ? "text-muted complete" : "Item__card-title"
-            } card-title`}
+              } card-title`}
           >
             Name: {props.full_name}
           </h4>
           <h4
             className={`${
               props.completed ? "text-muted complete" : "Item__card-title"
-            } card-title`}
+              } card-title`}
           >
             Email: {props.email}
           </h4>
           <h4
             className={`${
               props.completed ? "text-muted complete" : "Item__card-title"
-            } card-title`}
+              } card-title`}
           >
             Phone: {props.phone}
           </h4>
           <h4
             className={`${
               props.completed ? "text-muted complete" : "Item__card-title"
-            } card-title`}
+              } card-title`}
           >
             Address: {props.address}
           </h4>
           <h4
             className={`${
               props.completed ? "text-muted complete" : "Item__card-title"
-            } card-title`}
+              } card-title`}
           >
             Postcode: {props.postcode}
           </h4>
           <h4
             className={`${
               props.completed ? "text-muted complete" : "Item__card-title"
-            } card-title`}
+              } card-title`}
           >
             Zone: {props.zone}
           </h4>
@@ -61,21 +72,32 @@ function ListClients(props) {
           {props.completed ? (
             <h4 className="text-muted complete">Status: Delivered</h4>
           ) : (
-            <div>
-              <h4 className="Item__card-title">Status: Awaiting delivery</h4>
-              <small>Created {moment(props.createDate).calendar()}</small>
-            </div>
-          )}
+              <div>
+                <h4 className="Item__card-title">Status: Awaiting delivery</h4>
+                <small>Created {moment(props.createDate).calendar()}</small>
+              </div>
+            )}
         </div>
 
         <div className="card-footer text-muted bg-light">
           {!props.completed && (
+
             <button
               className="btn btn-sm mr-3 Item__button"
               type="button"
               onClick={handleCompleteClick}
             >
               <i className="fa fa-thumbs-up"></i>
+            </button>
+
+          )}
+          {!props.completed && (
+            <button
+              className="btn btn-sm mr-3 Item__button"
+              type="button"
+              onClick={handleDeliverClick}
+            >
+              <i className="fa fa-truck"></i>
             </button>
           )}
 
@@ -86,6 +108,8 @@ function ListClients(props) {
           >
             <i className="fa fa-trash"></i>
           </button>
+
+
         </div>
       </div>
     </div>
--- src/Components/signup.js
+++ src/Components/signup.js
@@ -5,104 +5,102 @@ import { Button } from 'react-bootstrap';
 
 
 // export default class SignUp extends Component {
-    function SignUp() {
+function SignUp() {
 
-        const [name, setName] = useState("");
-        const [email, setEmail] = useState("");
-        const [phone, setPhone] = useState("");
-        const [address, setAddress] = useState("");
-        const [postcode, setPostcode] = useState("");
-        const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [postcode, setPostcode] = useState("");
+  const [password, setPassword] = useState("");
 
-        function handleNameChange(event) {
-            setName(event.target.value);
-          }
-          function handleEmailChange(event) {
-            setEmail(event.target.value);
-          }
-          function handlePhoneChange(event) {
-            setPhone(event.target.value);
-          }
-          function handleAddressChange(event) {
-            setAddress(event.target.value);
-          }
-          function handlePostcodeChange(event) {
-            setPostcode(event.target.value);
-          }
-          function handlePasswordChange(event) {
-            setPassword(event.target.value);
-          }
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+  }
+  function handlePhoneChange(event) {
+    setPhone(event.target.value);
+  }
+  function handleAddressChange(event) {
+    setAddress(event.target.value);
+  }
+  function handlePostcodeChange(event) {
+    setPostcode(event.target.value);
+  }
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+  }
 
-          const history = useHistory();
-          function handleSubmitClick(event) {
-            event.preventDefault();
-            if (name === "") {
-              alert("Forgot the name");
-            } else if (email === "") {
-                alert("Forgot the email");
-                
-              }else if (phone === "") {
-                alert("Forgot the phone");
-                
-              }else if (address === "") {
-                alert("Forgot the address");
-                
-              }else if (postcode === "") {
-                alert("Forgot the postcode");
-                
-              }else if (password === "") {
-                alert("Forgot the password");
-                
-              }
-            
-            
-            else {
-              alert("Thank you for sign up");
-              
-              let path = "/sign-in";
-              history.push(path);
-            } 
-          }
-        
-        return (
-            <layer className="registration-form">
-                <form>
-                    <h3>Register To Volunteer</h3>
-                    <div className="form-group">
-                        <label>Name</label>
-                        <input type="text" className="form-control" placeholder="Full name" onChange={handleNameChange}/>
-                    </div>
-                    <div className="form-group">
-                        <label>Email address</label>
-                        <input type="email" className="form-control" placeholder="Email address " onChange={handleEmailChange} />
-                    </div>
-                    <div className="form-group">
-                        <label>Phone</label>
-                        <input type="phone" className="form-control" placeholder="Phone " onChange={handlePhoneChange}/>
-                    </div>
-                    <div className="form-group">
-                        <label>Address</label>
-                        <input type="text" className="form-control" placeholder="House number and street name"onChange={handleAddressChange} />
-                    </div>
-                    <div className="form-group">
-                        <label>Post code</label>
-                        <input type="text" className="form-control" placeholder="Post code" onChange={handlePostcodeChange}/>
-                    </div>
-                    
-                    <div className="form-group">
-                        <label>Password</label>
-                        <input type="password" className="form-control" placeholder="Create password" onChange={handlePasswordChange}/>
-                    </div>
+  const history = useHistory();
+  function handleSubmitClick(event) {
+    event.preventDefault();
+    if (name === "") {
+      alert("Forgot the name");
+    } else if (email === "") {
+      alert("Forgot the email");
 
-                    <div class="text-center" >
-                        <Button variant="primary" style={{ width: '100px' }}
-                        onClick={handleSubmitClick}><a href="/sign-in" style={{ color: 'white' }}>Sign Up</a></Button>
-                    </div>
-                    <p className=" text-right">
-                        Already registered <a href="/sign-in">sign in?</a>
-                    </p>
-                </form>
-            </layer>
-        );
+    } else if (phone === "") {
+      alert("Forgot the phone");
+
+    } else if (address === "") {
+      alert("Forgot the address");
+
+    } else if (postcode === "") {
+      alert("Forgot the postcode");
+
+    } else if (password === "") {
+      alert("Forgot the password");
+
     }
-    export default SignUp;
+
+
+    else {
+      alert("Thank you for sign up");
+
+      let path = "/sign-in";
+      history.push(path);
+    }
+  }
+
+  return (
+    <layer className="registration-form">
+      <form>
+        <h3>Register To Volunteer</h3>
+        <div className="form-group">
+          <label>Name</label>
+          <input type="text" className="form-control" placeholder="Full name" onChange={handleNameChange} />
+        </div>
+        <div className="form-group">
+          <label>Email address</label>
+          <input type="email" className="form-control" placeholder="Email address " onChange={handleEmailChange} />
+        </div>
+        <div className="form-group">
+          <label>Phone</label>
+          <input type="phone" className="form-control" placeholder="Phone " onChange={handlePhoneChange} />
+        </div>
+        <div className="form-group">
+          <label>Address</label>
+          <input type="text" className="form-control" placeholder="House number and street name" onChange={handleAddressChange} />
+        </div>
+        <div className="form-group">
+          <label>Post code</label>
+          <input type="text" className="form-control" placeholder="Post code" onChange={handlePostcodeChange} />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input type="password" className="form-control" placeholder="Create password" onChange={handlePasswordChange} />
+        </div>
+        <div class="text-center" >
+          <Button variant="primary" style={{ width: '100px' }}
+            onClick={handleSubmitClick}><a href="/sign-in" style={{ color: 'white' }}>Sign Up</a></Button>
+        </div>
+        <p className=" text-right">
+          Already registered <a href="/sign-in">sign in?</a>
+        </p>
+      </form>
+    </layer>
+  );
+}
+export default SignUp;
