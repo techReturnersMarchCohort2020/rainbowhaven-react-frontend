4b6cae54fa7d16d3a6892389c5ad8e51ca726986
Mariya <mariya.podosinova@gmail.com>
2020-07-10 17:05:01 +0100

    ListPickUpVolunteer page and styling

---
+++ src/Components/ManagerHome/ListPickUpVolunteer.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function ListPickUpVolunteer(props) {
+  return (
+    <div className="col-sm-12 col-md-6 col-lg-3">
+      <div className="form-group">
+        <ul className="list-group ">
+          <div className="custom-control custom-radio">
+            <input
+              type="radio"
+              id={props.volunteer_Id}
+              name="customRadio"
+              className="custom-control-input"
+            />
+            <label className="custom-control-label " for={props.volunteer_Id}>
+              {props.full_name}
+            </label>
+          </div>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default ListPickUpVolunteer;
