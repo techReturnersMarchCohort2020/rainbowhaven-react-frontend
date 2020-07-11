3bb0ffa784850332c567739b58514bcf7b600b25
Mariya <mariya.podosinova@gmail.com>
2020-07-11 23:30:52 +0100

    Updates

---
+++ precache-manifest.7172bcba6c3941a230a1e5ff852f1f28.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83f7bd7befca0ab5aeb41917821c3f45",
+    "url": "/rainbowhaven-react-frontend/index.html"
+  },
+  {
+    "revision": "3bb1f777c769a2752dae",
+    "url": "/rainbowhaven-react-frontend/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c13704efb488acd28b02",
+    "url": "/rainbowhaven-react-frontend/static/css/main.c92db21f.chunk.css"
+  },
+  {
+    "revision": "3bb1f777c769a2752dae",
+    "url": "/rainbowhaven-react-frontend/static/js/2.a3e895e1.chunk.js"
+  },
+  {
+    "revision": "031b62b957b4fb917cd0ec81e2c8bd7b",
+    "url": "/rainbowhaven-react-frontend/static/js/2.a3e895e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c13704efb488acd28b02",
+    "url": "/rainbowhaven-react-frontend/static/js/main.73c7223f.chunk.js"
+  },
+  {
+    "revision": "2c13713f72a702a353a8",
+    "url": "/rainbowhaven-react-frontend/static/js/runtime-main.d3c22bb4.js"
+  },
+  {
+    "revision": "c49e25750e4c721510e8818b15de26f4",
+    "url": "/rainbowhaven-react-frontend/static/media/bigBalloon.c49e2575.jpg"
+  }
+]);
