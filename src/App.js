5385cc0e568b9b9d56a5c4faad3c330e4c11013d
Mariya <mariya.podosinova@gmail.com>
2020-07-11 23:19:40 +0100

    final styling

--- src/App.js
+++ src/App.js
@@ -37,11 +37,11 @@ function App() {
                     For Volunteer
                   </Link>
                 </li>
-                {/* <li className="nav-item">
+                <li className="nav-item">
                   <Link className="nav-link" to={"/pickUp-Volunteer"}>
                     pickUpVolunteer
                   </Link>
-                </li> */}
+                </li>
               </ul>
             </div>
           </div>
