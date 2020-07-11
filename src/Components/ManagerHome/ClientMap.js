7c996aa074f4bf3f89a7dab66679448a7d89e0b1
Mariya <mariya.podosinova@gmail.com>
2020-07-11 23:25:24 +0100

    map styling changes

--- src/Components/ManagerHome/ClientMap.js
+++ src/Components/ManagerHome/ClientMap.js
@@ -1,69 +1,90 @@
-import React from "react"
-import { GoogleMap, withScriptjs, withGoogleMap, Marker} from "react-google-maps";
+import React from "react";
+import {
+  GoogleMap,
+  withScriptjs,
+  withGoogleMap,
+  Marker,
+} from "react-google-maps";
 import Geocode from "react-geocode";
 
 function Map() {
+  //   Geocode.setApiKey("");
+  //   // set response language. Defaults to english.
+  // Geocode.setLanguage("en");
+  // // Get latidude & longitude from address.
+  // Geocode.fromAddress("33 cambridge street").then(
+  //   response => {
+  //     const { lat, lng } = response.results[0].geometry.location;
+  //     console.log(lat, lng);
+  //   },
+  //   error => {
+  //     console.error(error);
+  //   }
+  // );
 
-//   Geocode.setApiKey("");
-//   // set response language. Defaults to english.
-// Geocode.setLanguage("en");
-// // Get latidude & longitude from address.
-// Geocode.fromAddress("33 cambridge street").then(
-//   response => {
-//     const { lat, lng } = response.results[0].geometry.location;
-//     console.log(lat, lng);
-//   },
-//   error => {
-//     console.error(error);
-//   }
-// );
-
-  return ( 
-    <GoogleMap 
-      defaultZoom={12} 
-      defaultCenter={{lat: 53.483959, lng: -2.244644}}
-      
+  return (
+    <GoogleMap
+      defaultZoom={12}
+      defaultCenter={{ lat: 53.483959, lng: -2.244644 }}
     >
       <Marker
-       icon={{url: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'}}
-      position={{ lat: 53.483959, lng: -2.244644 }}
-      label={{fontSize: '15px',text : "Tom",color: "white",fontWeight: "bold"}}
-    />
-    <Marker
-       icon={{url: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'}}
-      position={{ lat: 53.468820, lng: -2.156210 }}
-      label={{fontSize: '15px',text : "Tom",color: "white",fontWeight: "bold"}}
-    />
-   
-    <Marker 
-    icon={{url: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'}}
-      position={{ lat: 53.450714, lng: -1.994911 }}
-      label={{fontSize: '15px',text : "Tom",color: "white",fontWeight: "bold"}}
-    />
-    <Marker 
-    icon={{url: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'}}
-      position={{ lat: 53.482800, lng: -2.204350 }} 
-      label={{fontSize: '15px',text : "Tom",color: "white",fontWeight: "bold"}}
-    />
+        icon={{ url: "http://maps.google.com/mapfiles/ms/icons/red-dot.png" }}
+        position={{ lat: 53.483959, lng: -2.244644 }}
+        label={{
+          fontSize: "15px",
+          text: "Tom",
+          color: "white",
+          fontWeight: "bold",
+        }}
+      />
+      <Marker
+        icon={{ url: "http://maps.google.com/mapfiles/ms/icons/red-dot.png" }}
+        position={{ lat: 53.46882, lng: -2.15621 }}
+        label={{
+          fontSize: "15px",
+          text: "Tom",
+          color: "white",
+          fontWeight: "bold",
+        }}
+      />
 
-  </GoogleMap>
+      <Marker
+        icon={{ url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png" }}
+        position={{ lat: 53.450714, lng: -1.994911 }}
+        label={{
+          fontSize: "15px",
+          text: "Tom",
+          color: "white",
+          fontWeight: "bold",
+        }}
+      />
+      <Marker
+        icon={{ url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png" }}
+        position={{ lat: 53.4828, lng: -2.20435 }}
+        label={{
+          fontSize: "15px",
+          text: "Tom",
+          color: "white",
+          fontWeight: "bold",
+        }}
+      />
+    </GoogleMap>
   );
 }
 
 const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 function ClientMap() {
-    return (
-      <div style={{width: '70vw', height: '60vh'}}>
-        <WrappedMap 
-        googleMapURL = {`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places`}
+  return (
+    <div>
+      <WrappedMap
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places`}
         loadingElement={<div style={{ height: `150%` }} />}
         containerElement={<div style={{ height: `600px` }} />}
-        mapElement={<div style={{ height: `90%` }} />} />
-      </div>
-    )
-  }
+        mapElement={<div style={{ height: `90%` }} />}
+      />
+    </div>
+  );
+}
 
 export default ClientMap;
-
-
